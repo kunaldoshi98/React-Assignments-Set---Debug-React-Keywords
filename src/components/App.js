bb8a2cf2588c0bbec933c6ada1d6336de3262853
kunaldoshi98 <68844308+kunaldoshi98@users.noreply.github.com>
2020-11-25 20:39:19 +0530

    try

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
-  let [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
   return (
     <div class="ball">
